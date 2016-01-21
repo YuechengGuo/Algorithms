f327cabfbbd0e113551746ce8f39befe29db933d
Yuecheng Guo <gyc8990@gmail.com>
2016-01-20 21:33:05 -0500

    upload tests for binary search

--- section1/BinarySearch.java
+++ section1/BinarySearch.java
@@ -5,15 +5,25 @@ import java.util.*;
 public class BinarySearch {
 	public static int rank(int key, int[] a){
 		int lo = 0;
-		int hi = 0;
+		int hi = a.length;
+		while (lo < hi) {
+			int mid = lo + (hi - lo) / 2;
+			if (key < a[mid]) hi = mid - 1;
+			else if (key > a[mid]) lo = mid + 1;
+			else return mid;
+		}
 		return hi;
 	}
 	
 	public static void main(String[] args) {
 		int[] numList = {1, 2, 4, 1, 2, 6, 7, 8};
-		int key = 5;
+		int key = numList[0];
 		Arrays.sort(numList);
-		display(numList);		
+		display(numList);
+		int index = rank(key, numList);
+		if (index != -1) {
+			System.out.println(index);
+		}
 		
 	}
 	
